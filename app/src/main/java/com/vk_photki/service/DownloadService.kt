40121edbcb7f5e83b9df0600697c625dc45b0c9b
Ilya Melnikov <i486dx@yandex.ru>
2015-04-08 00:00:36 +0400

    service

---
+++ app/src/main/java/com/vk_photki/service/DownloadService.kt
@@ -0,0 +1,5 @@
+package com.vk_photki.service
+
+/**
+ * Created by nightrain on 4/7/15.
+ */
---
+++ app/src/main/java/com/vk_photki/service/ServiceBinder.java
@@ -0,0 +1,7 @@
+package com.vk_photki.service;
+
+/**
+ * Created by nightrain on 4/7/15.
+ */
+public class ServiceBinder {
+}
